ec20054459f5f49415f8917de48b778eb03dc4eb
Rizvij <rizvi.juanid@gmail.com>
2023-03-09 22:52:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+
+app=Flask(__name__)
+
+
+@app.route("/") #localhost:port_number/ome
+##localhost:port_number/resultpage
+
+
+def home():
+    return "our first flask application"
+
+
+if __name__=="__main__":
+    app.run(debug=True) #port_number,option,host
+
